364ea64dfbefdea55716feef537ec0355f3fd1f5
Luis Navarrete <89224906+LAN-01@users.noreply.github.com>
2022-04-24 23:57:54 -0700

    Upload drawing code

---
+++ hangman.asm
@@ -0,0 +1,39 @@
+.data
+
+start: .asciiz " +---+       \n     |       \n     |       \n     |     \n------"
+head: .asciiz "\n +---+       \n O   |       \n     |       \n     |     \n------"
+body: .asciiz "\n +---+       \n O   |       \n |   |       \n     |     \n------"
+leftArm: .asciiz "\n +---+       \n O   |      \n/|   |       \n     |     \n------"
+rightArm: .asciiz "\n +---+       \n O   |       \n/|\\  |       \n     |     \n------"
+leftLeg: .asciiz "\n +---+       \n O   |       \n/|\\  |       \n/    |     \n------"
+rightLeg: .asciiz "\n +---+       \n O   |       \n/|\\  |       \n/ \\  |     \n------"
+
+.text
+main: 
+la $a0, start 
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, head
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, body
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, leftArm
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, rightArm
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, leftLeg
+li $v0, 4	# syscall to display string
+syscall
+
+la $a0, rightLeg
+li $v0, 4	# syscall to display string
+syscall
