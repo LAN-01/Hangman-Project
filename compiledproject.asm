b3e03aaa1a1362a0ff984578f6e696eac2ba2ae8
chrxstixnn <89479044+chrxstixnn@users.noreply.github.com>
2022-05-09 20:44:54 -0700

    Add files via upload

--- compiledproject.asm
+++ compiledproject.asm
@@ -7,9 +7,12 @@
   msg1: .asciiz "\nEnter a character: "
   msg2: .asciiz "\nYour character appears this many times in the word: "
   msg3: .asciiz "\n"
-  key: .asciiz "test"
-  key2: .asciiz "test"
+  complete: "Congrats! The word is: "
+  key: .asciiz "supercali"
+  key2: .asciiz "supercali"
   userInput: .space 2
+  beginningMessage: .asciiz "Welcome to Hangman! Enter letters that make up the secret word."
+  failedMsg: .asciiz "Sorry you lose!"
   
  start: .asciiz " +---+       \n     |       \n     |       \n     |     \n------"
 head: .asciiz "\n +---+       \n O   |       \n     |       \n     |     \n------"
@@ -20,14 +23,19 @@ leftLeg: .asciiz "\n +---+       \n O   |       \n/|\\  |       \n/    |     \n-
 rightLeg: .asciiz "\n +---+       \n O   |       \n/|\\  |       \n/ \\  |     \n------"
 
 
- .text 
+.text 
 
 li $s0, 0
 
+la $a0, beginningMessage
+li $v0, 4
+syscall
+
 main:
-  
- beq $s0, 7, exit
- la $s1, correctarray  #load address of correct array
+ 
+ li $t0, 0
+ beq $s7, 7, exit
+ la $t1, correctarray  #load address of correct array
  
  la $t7, 0
 
@@ -47,9 +55,6 @@ syscall
 
 
 
-
-
-
 compareGuess:
 la $s2, key	#$s2 = keyword
 move $t2, $s2
@@ -62,11 +67,12 @@ lb $t3, ($s3)
 	beq $t2,$t3, L2	#if characters are equal, jump to L2
 	beq $t2,$zero, count #if index = zero(non element), jump to exit
 	addi $s2,$s2,1	#else, continue itteration
+	addi $t0, $t0,1
 	j L1
 	L2:
 	addi $t7,$t7,1	#records that character appears in the word
 	addi $s2,$s2,1
-	 
+	addi $t0, $t0, 1
 	j L1
  
 
@@ -77,19 +83,28 @@ lb $t3, ($s3)
 
 
 count: 	#Stores correct letter in array
-
-	la $t4, key2
+	
+	la $a0, msg2 #character appearance message
+        li $v0, 4
+        syscall
+        la $a0,($t7)
+        li $v0,1
+        syscall
+        
+        
+	la $t4, key
 	bgt $t7, $zero, addinto
-	addi $s7, $s7, 1
+	addi $s7, $s7, 1 
 	j exitloop
 	
 addinto:
-	add $t0, $s1, $s0  #index of array
+	move $t2, $t0
+	add $t0, $t1, $s0  #index of array
         sb $t3, ($t0)   #store byte 
         
         addi $s0, $s0, 1  #update index of the array
-        addi $s6, $s6, 1  #size of the array
-        beq $s6, 3, exit
+        add $s6, $s6, $t7  #size of the array
+        beq $s6, $t2, printWhole
         
        la $a0, msg3
        li $v0, 4
@@ -97,10 +112,12 @@ addinto:
        
        
        li $t8, 0
+  	li $t0, 0
 
  firstloop: 
+ 	   
  	    #loop through the word
- 	    bgt $t8, 3, exitloop  
+ 	    beq $t8, $t2, exitloop  
   	    #new address
   	    add $t5, $t4, $t8
             lb $s4, ($t5)
@@ -110,7 +127,7 @@ addinto:
              #loop through the correctarray
  innerloop:  beq $t9, $s6, endinnerloop     
   	     
-  	     add $t6, $s1, $t9
+  	     add $t6, $t1, $t9
   	     lb $s5, ($t6)
   	     addi $t9, $t9, 1
   	     #checks if letter in array is equal to 
@@ -135,6 +152,28 @@ addinto:
   addi $t8, $t8, 1
   
   j firstloop
+  
+  
+  printWhole:
+  
+  la $a0, msg3
+  li $v0, 4
+  syscall
+  
+  la $a0, complete
+  li $v0, 4
+  syscall
+  
+
+  la $a0, key
+  li $v0, 4
+  syscall
+  
+  la $a0, msg3
+  li $v0, 4
+  syscall
+  j exit
+  
   
  exitloop:
  
@@ -191,14 +230,12 @@ part7:la $a0, rightLeg
 li $v0, 4	
 syscall
 
+la $a0, failedMsg
+li $v0,4
+syscall
+
 exit:
  
-la $a0, msg2 #character appearance message
-li $v0, 4
-syscall
-la $a0,($t7)
-li $v0,1
-syscall
  
  li $v0, 10
  syscall
